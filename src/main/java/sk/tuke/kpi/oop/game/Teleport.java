31b480961c4f98c44ab971c14b265ef0dbaef9e7
Roland Körtvely <mikronum@gmail.com>
2018-10-25 00:33:57 +0200

    Teleport update

--- src/main/java/sk/tuke/kpi/oop/game/Teleport.java
+++ src/main/java/sk/tuke/kpi/oop/game/Teleport.java
@@ -1,11 +1,50 @@
 package sk.tuke.kpi.oop.game;
 
 import sk.tuke.kpi.gamelib.framework.AbstractActor;
+import sk.tuke.kpi.gamelib.framework.Player;
 import sk.tuke.kpi.gamelib.graphics.Animation;
 
 public class Teleport extends AbstractActor {
 
-    public Teleport() {
+    private Teleport destination;
+
+    public Teleport()
+    {
+        this(null);
+    }
+
+    public Teleport(Teleport destination)
+    {
+        this.setDestination(destination);
         setAnimation(new Animation("sprites/lift.png", 48, 48));
+    }
+
+    public void teleportPlayer()
+    {
+        if (this.getDestination() == null) {
+            return;
+        }
+
+        Player player = this.getScene().getFirstActorByType(Player.class);
+        if (player == null) {
+            return;
+        }
+
+        player.setPosition(this.getDestination().getPosX(), this.getDestination().getPosY());
+    }
+
+    public Teleport getDestination()
+    {
+        return destination;
+    }
+
+    public void setDestination(Teleport destination)
+    {
+        if (this.equals(destination)) {
+            this.destination = null;
+            return;
+        }
+
+        this.destination = destination;
     }
 }
