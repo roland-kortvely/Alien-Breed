4048072de76038fdb2e107948a25129d47246b99
Roland Körtvely <mikronum@gmail.com>
2018-11-02 16:47:22 +0100

    GameLib update

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 group = "sk.tuke.kpi.oop"
 version = "1.0"
 
-val gamelibVersion = "2.2.0"
+val gamelibVersion = "2.3.0"
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_10
