f5999cdfe1b5368aa6a1a32c300ab42be022ffbb
Roland Körtvely <mikronum@gmail.com>
2018-11-04 11:11:56 +0100

    Arena...IX

--- src/main/java/sk/tuke/kpi/oop/game/tools/BreakableTool.java
+++ src/main/java/sk/tuke/kpi/oop/game/tools/BreakableTool.java
@@ -1,9 +1,10 @@
 package sk.tuke.kpi.oop.game.tools;
 
+import sk.tuke.kpi.gamelib.Actor;
 import sk.tuke.kpi.gamelib.Scene;
 import sk.tuke.kpi.gamelib.framework.AbstractActor;
 
-public abstract class BreakableTool<T> extends AbstractActor implements Usable<T> {
+public abstract class BreakableTool<T extends Actor> extends AbstractActor implements Usable<T> {
 
     private int remainingUses;
 
